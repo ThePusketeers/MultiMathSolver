f90628ebd4a93dade7e5fab01a2fdfa148985c75
BIBLETUM <pasha.remizov@mail.ru>
2024-03-31 16:27:15 +0300

    feat: example, how to extract business logic to repository

---
+++ app/src/main/java/com/example/multimathsolver/domain/GetPerfectDNFUseCase.java
@@ -0,0 +1,15 @@
+package com.example.multimathsolver.domain;
+
+import com.example.multimathsolver.data.booleanalgebra.BooleanFunction;
+
+public class GetPerfectDNFUseCase {
+    private final Repository repository;
+
+    public GetPerfectDNFUseCase(Repository repository) {
+        this.repository = repository;
+    }
+
+    public String getPerfectDNFUseCase(BooleanFunction b) {
+        return repository.perfectDNF(b);
+    }
+}
