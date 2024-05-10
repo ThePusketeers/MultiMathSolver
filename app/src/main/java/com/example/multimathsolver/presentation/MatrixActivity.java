2fa2592f5769095b95d086aeb3cd7883b8c75ce1
robl0xx <robertromanov2018@mail.ru>
2024-05-10 22:29:20 +0300

    feat: try to add matrixMapper, add some buttons

--- app/src/main/java/com/example/multimathsolver/presentation/MatrixActivity.java
+++ app/src/main/java/com/example/multimathsolver/presentation/MatrixActivity.java
@@ -5,25 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.evrencoskun.tableview.TableView;
 import com.example.multimathsolver.R;
+import com.example.multimathsolver.domain.MatrixOperations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class MatrixActivity extends AppCompatActivity {
 
     private List<String> mRowHeaderList = new ArrayList<>();
     private List<String> mColumnHeaderList = new ArrayList<>();
-    ;
     private List<List<String>> mCellList = new ArrayList<>();
-    private Button btn;
-    ;
+    private Button matrixDisplayButton;
+    private Button determinantCountButton;
     private TableView tableView;
+    private MatrixOperations matrix;
+    private TextView determinantAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,9 @@ public class MatrixActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
 
-        btn = findViewById(R.id.add_item_button);
+        matrixDisplayButton = findViewById(R.id.add_item_button);
+        determinantCountButton = findViewById(R.id.get_determinant_button);
+        determinantAnswer = findViewById(R.id.determinant_display);
 
         tableView = findViewById(R.id.table_view);
 
@@ -41,34 +47,45 @@ public class MatrixActivity extends AppCompatActivity {
         // Set this adapter to the our TableView
         tableView.setAdapter(adapter);
 
+        matrixDisplayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double[][] matrixAsArray = new double[][] { {1, 2, 3, 4, 5}, {4, 8, 3, 9, 1},{5, 1, 8, 2, 1}, {9, 22, 13, 7, 4} };
+                for (int i = 0; i < matrixAsArray.length; i++) {
 
-        Object[][] testMatrix = new Object[][]{
-                new double[][] {{1, 2, 3, 4, 5}, {4, 8, 3, 9, 1}},
-                new double[][] {{5, 1, 8, 2, 1}, {9, 22, 13, 7, 4}},
-                new double[][] {{6, 3, 11, 6, 6}, {13, 30, 16, 16, 5}}};
+                    List<String> temp = new ArrayList<>();
 
-        for (int i = 0; i < testMatrix.length; i++) {
+                    for (int j = 0; j < matrixAsArray[0].length; j++) {
+                        temp.add(String.valueOf(matrixAsArray[i][j]));
+                    }
 
-            List<String> temp = new ArrayList<>();
+                    mCellList.add(temp);
+                }
 
-            for (int j = 0; j < testMatrix.length; j++) {
-                temp.add(String.valueOf(  ));
+                // Let's set datas of the TableView on the Adapter
+                adapter.setAllItems(mColumnHeaderList, mRowHeaderList, mCellList);
+
             }
+        });
 
-            mCellList.add(temp);
-        }
-
-
-        // Let's set datas of the TableView on the Adapter
-        adapter.setAllItems(mColumnHeaderList, mRowHeaderList, mCellList);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        determinantCountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
 
+    }
+
+    public static double[][] matrixMapper(MyTableViewAdapter myTableViewAdapter){
+
+    double[][] resultMatrix = new double[4][5];
+        for (int i = 0; i < resultMatrix.length; i++) {
+            for (int j = 0; j < resultMatrix[0].length; j++) {
+                resultMatrix[i][j] = Double.parseDouble(Objects.requireNonNull(myTableViewAdapter.getCellItem(i, j)));
+            }
+        }
+        return resultMatrix;
     }
 
     public static Intent newIntent(Context context) {
