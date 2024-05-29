19393063dadee946dc07de90db8ffcea2ca68743
robl0xx <robertromanov2018@mail.ru>
2024-05-29 17:56:46 +0300

    feat: change "id" of buttons and initialized them in MatrixActivity, connect activity to navigation menu bar, improve structure of MatrixActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
-
     implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
-
+    implementation ("com.github.evrencoskun:TableView:v0.8.9.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/multimathsolver/presentation/MatrixActivity.java
+++ app/src/main/java/com/example/multimathsolver/presentation/MatrixActivity.java
@@ -1,10 +1,13 @@
 package com.example.multimathsolver.presentation;
 
+import static com.example.multimathsolver.presentation.LimitActivity.newIntentLimit;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.evrencoskun.tableview.TableView;
 import com.example.multimathsolver.R;
 import com.example.multimathsolver.domain.MatrixOperations;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,27 +23,37 @@ import java.util.Objects;
 
 
 public class MatrixActivity extends AppCompatActivity {
-
     private List<String> mRowHeaderList = new ArrayList<>();
     private List<String> mColumnHeaderList = new ArrayList<>();
     private List<List<String>> mCellList = new ArrayList<>();
     private Button matrixDisplayButton;
     private Button determinantCountButton;
+    private Button rangCountButton;
+    private Button multiplyMatrixButton;
+    private Button sumMatrixButton;
+    private Button multiplyByConstantMatrixButton;
+    private Button raiseToDegreeMatrixButton;
+    private Button subtractMatrixButton;
+    private EditText degreeInputField;
+    private EditText constantInputField;
+    private Button saveToMatrix_A_Button;
+    private Button saveToMatrix_B_Button;
     private TableView tableView;
     private MatrixOperations matrix;
     private TextView determinantAnswer;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
+        initViews();
 
-        matrixDisplayButton = findViewById(R.id.add_item_button);
-        determinantCountButton = findViewById(R.id.get_determinant_button);
-        determinantAnswer = findViewById(R.id.determinant_display);
+        navigationView.setSelectedItemId(R.id.matrix_menu);
+        setUpOnClickListeners();
+        setUpOnItemListeners();
 
-        tableView = findViewById(R.id.table_view);
 
         // Create our custom TableView Adapter
         MyTableViewAdapter adapter = new MyTableViewAdapter();
@@ -74,6 +88,50 @@ public class MatrixActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private void setUpOnClickListeners() {
+
+    }
+
+    private void setUpOnItemListeners() {
+        navigationView.setOnItemSelectedListener(item -> {
+            final int id = item.getItemId();
+            if (id == R.id.matrix_menu) {
+                return true;
+            } else if (id == R.id.slay_menu) {
+                startActivity(new Intent(MatrixActivity.this, MainActivity2.class)); // заменить MainActivity2 на класс для СЛАУ
+                finish();
+                return true;
+            } else if (id == R.id.limit_menu) {
+                startActivity(new Intent(MatrixActivity.this, LimitActivity.class)); // заменить MainActivity2 на класс для Пределов
+                finish();
+                return true;
+            } else if (id == R.id.discra_menu) {
+                startActivity(new Intent(MatrixActivity.this, BooleanAlgebraActivity.class)); // заменить MainActivity2 на класс для Дискры
+                finish();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void initViews(){
+        matrixDisplayButton = findViewById(R.id.add_item_button);
+        determinantCountButton = findViewById(R.id.get_determinant_button);
+        determinantAnswer = findViewById(R.id.determinant_display);
+        rangCountButton = findViewById(R.id.get_rang_button);
+        tableView = findViewById(R.id.table_view);
+        multiplyMatrixButton = findViewById(R.id.multiply_matrix);
+        sumMatrixButton = findViewById(R.id.sum_matrix);
+        multiplyByConstantMatrixButton = findViewById(R.id.multiply_by_constant_matrix);
+        raiseToDegreeMatrixButton = findViewById(R.id.raise_to_degree_matrix);
+        subtractMatrixButton = findViewById(R.id.subtract_matrix);
+        degreeInputField = findViewById(R.id.degree_input_field);
+        constantInputField = findViewById(R.id.constant_input_field);
+        saveToMatrix_A_Button = findViewById(R.id.save_to_matrix_A_button);
+        saveToMatrix_B_Button = findViewById(R.id.save_to_matrix_B_button);
 
     }
 
